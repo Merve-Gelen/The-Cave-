e0ff8be8c5982b05493c1276917a3d07506090c7
Merve-Gelen <158482821+Merve-Gelen@users.noreply.github.com>
2024-03-27 22:23:34 +0300

    Hareketli Platformlar
    Camera ve Player ayarları yeniden yapıldı.

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float FollowSpeed = 2f;
     public Transform target;
+    public float cameraSpeed;
 
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, target.position.y, - 10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        
+        transform.position = Vector3.Slerp(transform.position, new Vector3(target.position.x, target.position.y, transform.position.z), cameraSpeed);
     }
 }
